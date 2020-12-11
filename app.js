da581a916bfd485ec8c5b743bc6db4aa54662119
Branislav Buna <graphicglobal67@gmail.com>
2020-12-11 13:14:50 +0000

    added tutorial on the start, refactored rotation of the certain objects, displayed board in focused box, showing alerts when taken certain actions

--- app.js
+++ app.js
@@ -4,6 +4,128 @@ let previous_name = [];
 let bid_namer = document.getElementById('bid_name');
 let bid_value = document.getElementById('bid_value');
 let select_slot = document.getElementById('sel1');
+let notification = document.querySelector('.notification');
+let main_canvas = document.querySelector('.main_canvas');
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+window.addEventListener('load', () => {
+    
+    if(localStorage.getItem('tutorial') == 'finished') {
+        notification.style.opacity = "1";
+        notification.style.transition = "all .5s ease";
+        notification.textContent = "Welcome in Table Maker";
+
+        setTimeout(() => {
+            notification.style.opacity = "0";
+            notification.style.transition = "all .5s ease";
+            notification.textContent = "";
+        }, 2000);
+    }else {
+        notification.style.opacity = "1";
+        notification.style.transition = "all .5s ease";
+        notification.textContent = "Tutorial for Table Maker started !";
+
+        setTimeout(() => {
+            notification.style.opacity = "0";
+            notification.style.transition = "all .5s ease";
+            notification.textContent = "";
+        }, 2000);
+
+        tippy('#square', {
+            content: 'This is squared table',
+            showOnCreate: true,
+            animation: 'fase',
+            onShow(instance) {
+                setTimeout(() => {
+                    instance.hide();
+                    tippy('#circle', {
+                        content: 'This is circle table',
+                        showOnCreate: true,
+                        animation: 'fase',
+                        onShow(instance) {
+                            setTimeout(() => {
+                                instance.hide();
+                                tippy('#twosquare', {
+                                    content: 'This is double table',
+                                    showOnCreate: true,
+                                    animation: 'fase',
+                                    onShow(instance) {
+                                        setTimeout(() => {
+                                            instance.hide();
+                                            tippy('#window', {
+                                                content: 'This is window',
+                                                showOnCreate: true,
+                                                animation: 'fase',
+                                                onShow(instance) {
+                                                    setTimeout(() => {
+                                                        instance.hide();
+                                                        tippy('#door', {
+                                                            content: 'These are doors',
+                                                            showOnCreate: true,
+                                                            animation: 'fase',
+                                                            onShow(instance) {
+                                                                setTimeout(() => {
+                                                                    instance.hide();
+                                                                    tippy('#rota', {
+                                                                        content: 'Use this arrow to rotate objects',
+                                                                        showOnCreate: true,
+                                                                        animation: 'fase',
+                                                                        onShow(instance) {
+                                                                            setTimeout(() => {
+                                                                                instance.hide();
+                                                                                tippy('#floor', {
+                                                                                    content: 'This is restaurant floor',
+                                                                                    showOnCreate: true,
+                                                                                    animation: 'fase',
+                                                                                    onShow(instance) {
+                                                                                        setTimeout(() => {
+                                                                                            instance.hide();
+                                                                                            tippy('#tool_menu', {
+                                                                                                content: 'Hold and move the objects to restaurant floor on the right side',
+                                                                                                showOnCreate: true,
+                                                                                                animation: 'fase',
+                                                                                                onShow(instance) {
+                                                                                                    setTimeout(() => {
+                                                                                                        instance.hide();
+                                                                                                        notification.style.opacity = "1";
+                                                                                                        notification.style.transition = "all .5s ease";
+                                                                                                        notification.textContent = "Tutorial Finished !";
+                                                                                                        setTimeout(() => {
+                                                                                                            notification.style.opacity = "0";
+                                                                                                            notification.style.transition = "all .5s ease";
+                                                                                                            notification.textContent = "";
+                                                                                                        }, 2000);
+                                                                                                        localStorage.setItem('tutorial', 'finished');
+                                                                                                        window.location = window.location;
+                                                                                                    }, 5000);
+                                                                                                }
+                                                                                            });
+                                                                                        }, 5000);
+                                                                                    }
+                                                                                });
+                                                                            }, 5000);
+                                                                        }
+                                                                    });
+                                                                }, 5000);
+                                                            }
+                                                        });
+                                                    }, 5000);
+                                                }
+                                            });
+                                        }, 5000);
+                                    }
+                                });
+                            }, 5000);
+                        }
+                    });
+                }, 5000);
+            }
+        });
+    }
+});
 
 function bid_add(){
     let opt = select_slot.options[select_slot.selectedIndex];
@@ -21,40 +143,56 @@ function bid_add(){
         booked5.innerHTML = 'Yes';
         name5.innerHTML = bid_namer.value;
         bid5.innerHTML = bid_value.value;
+
+        notification.style.opacity = "1";
+        notification.style.transition = "all .5s ease";
+        notification.textContent = `Table booked for ${bid_namer.value} with bid ${bid_value.value}€`;
+
+        setTimeout(() => {
+            notification.style.opacity = "0";
+            notification.style.transition = "all .5s ease";
+            notification.textContent = "";
+        }, 2000);
     }else if(opt.value == '7'){
         booked7.innerHTML = 'Yes';
         name7.innerHTML = bid_namer.value;
         bid7.innerHTML = bid_value.value;
+
+        notification.style.opacity = "1";
+        notification.style.transition = "all .5s ease";
+        notification.textContent = `Table booked for ${bid_namer.value} with bid ${bid_value.value}€`;
+
+        setTimeout(() => {
+            notification.style.opacity = "0";
+            notification.style.transition = "all .5s ease";
+            notification.textContent = "";
+        }, 2000);
     }else if(opt.value == '9'){
         booked9.innerHTML = 'Yes';
         name9.innerHTML = bid_namer.value;
         bid9.innerHTML = bid_value.value;
+
+        notification.style.opacity = "1";
+        notification.style.transition = "all .5s ease";
+        notification.textContent = `Table booked for ${bid_namer.value} with bid ${bid_value.value}€`;
+
+        setTimeout(() => {
+            notification.style.opacity = "0";
+            notification.style.transition = "all .5s ease";
+            notification.textContent = "";
+        }, 2000);
     }
 
     previous_bid.push(Number(bid_value.value));
     previous_name.push(bid_namer.value);
 
-    // for(let i = 0; i < previous_bid.length; i++){
-    //     if(previous_bid[i] < Number(bid_value.value)){
-    //         for(let j = 0; j < previous_name.length; j++){
-    //             alert(previous_name[i] + ' was bid out');
-    //         }
-    //     }
-    // }
-
     bid_namer.value = '';
     bid_value.value = '';
+    $('#exampleModal').modal('hide');
 }
 
 function clicked(event) {
     let new_id = event.target.id;
-    if(event.target.classList.contains('twosquared')){
-        event.target.classList.toggle('rotated');
-    }else if(event.target.classList.contains('window')){
-        event.target.classList.toggle('rotated');
-    }else if(event.target.classList.contains('door')){
-        event.target.classList.toggle('rotated');
-    }
 
     let obj = {
         table: {
@@ -116,11 +254,24 @@ function clicked(event) {
     }
 }
 
+function rotate(event) {
+    event.stopPropagation();
+    let el = event.target.parentNode;
+    if(el.classList.contains('twosquared')){
+        el.classList.toggle('rotated');
+    }else if(el.classList.contains('window')){
+        el.classList.toggle('rotated');
+    }else if(el.classList.contains('door')){
+        el.classList.toggle('rotated');
+    }
+}
+
 function drag_start(event) {
     event.target.style.cursor = 'grabbing';
     let style = window.getComputedStyle(event.target, null);
     let str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY) + ',' + event.target.id;
     event.dataTransfer.setData("Text", str);
+    event.target.setAttribute('onClick','clicked(event)');
 }
 
 function drop(event) {
@@ -141,6 +292,31 @@ function drop(event) {
     dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
     dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
     event.preventDefault();
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    notification.style.opacity = "1";
+    notification.style.transition = "all .5s ease";
+    if(dm.classList.contains('fa-square-full')) {
+        notification.textContent = "Square Table added successfully";
+    }else if(dm.classList.contains('fa-circle')){
+        notification.textContent = "Circle Table added successfully";
+    }else if(dm.classList.contains('twosquared')){
+        notification.textContent = "Large Table added successfully";
+    }else if(dm.classList.contains('window')){
+        notification.textContent = "Window added successfully";
+    }else if(dm.classList.contains('door')){
+        notification.textContent = "Window added successfully";
+    }
+
+    setTimeout(() => {
+        notification.style.opacity = "0";
+        notification.style.transition = "all .5s ease";
+        notification.textContent = "";
+    }, 2000);
+
     return false;
 }
 
@@ -156,7 +332,6 @@ const new_square = () => {
     new_el.setAttribute('id', Math.floor(Math.random() * 1000));
     new_el.setAttribute('draggable', 'true');
     new_el.setAttribute('ondragstart', 'drag_start(event)');
-    new_el.setAttribute('onclick', 'clicked(event)');
     tool_menu.appendChild(new_el);
 }
 
@@ -167,7 +342,6 @@ const new_circle = () => {
     new_el.setAttribute('id', Math.floor(Math.random() * 1000));
     new_el.setAttribute('draggable', 'true');
     new_el.setAttribute('ondragstart', 'drag_start(event)');
-    new_el.setAttribute('onclick', 'clicked(event)');
     tool_menu.appendChild(new_el);
 }
 
@@ -178,8 +352,15 @@ const new_double_square = () => {
     new_el.setAttribute('id', Math.floor(Math.random() * 1000));
     new_el.setAttribute('draggable', 'true');
     new_el.setAttribute('ondragstart', 'drag_start(event)');
-    new_el.setAttribute('onclick', 'clicked(event)');
     tool_menu.appendChild(new_el);
+
+    let rotate_arrow = document.createElement('i');
+    rotate_arrow.setAttribute('class', 'fas fa-undo');
+    rotate_arrow.setAttribute('onclick', 'rotate(event)');
+    rotate_arrow.setAttribute('data-toggle', 'tooltip');
+    rotate_arrow.setAttribute('data-placement', 'top');
+    rotate_arrow.setAttribute('title', 'Rotate Double Table');
+    new_el.appendChild(rotate_arrow);
 }
 
 // generating new element after drop
@@ -190,8 +371,15 @@ const new_window = () => {
     new_el.textContent = 'WINDOW';
     new_el.setAttribute('draggable', 'true');
     new_el.setAttribute('ondragstart', 'drag_start(event)');
-    new_el.setAttribute('onclick', 'clicked(event)');
     tool_menu.appendChild(new_el);
+
+    let rotate_arrow = document.createElement('i');
+    rotate_arrow.setAttribute('class', 'fas fa-undo');
+    rotate_arrow.setAttribute('onclick', 'rotate(event)');
+    rotate_arrow.setAttribute('data-toggle', 'tooltip');
+    rotate_arrow.setAttribute('data-placement', 'top');
+    rotate_arrow.setAttribute('title', 'Rotate Window');
+    new_el.appendChild(rotate_arrow);
 }
 
 // generating new element after drop
@@ -202,7 +390,13 @@ const new_doors = () => {
     new_el.textContent = 'DOORS';
     new_el.setAttribute('draggable', 'true');
     new_el.setAttribute('ondragstart', 'drag_start(event)');
-    new_el.setAttribute('onclick', 'clicked(event)');
     tool_menu.appendChild(new_el);
-}
 
+    let rotate_arrow = document.createElement('i');
+    rotate_arrow.setAttribute('class', 'fas fa-undo');
+    rotate_arrow.setAttribute('onclick', 'rotate(event)');
+    rotate_arrow.setAttribute('data-toggle', 'tooltip');
+    rotate_arrow.setAttribute('data-placement', 'top');
+    rotate_arrow.setAttribute('title', 'Rotate Doors');
+    new_el.appendChild(rotate_arrow);
+}
